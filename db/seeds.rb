a89f6da0e2bbdefb4c2bcab8ea6853466c904cf5
Pierre Jean Heissat <pjn.heissat@gmail.com>
2019-12-19 21:54:43 +0100

    Exercice en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+Flat.create!(
+  name: 'Grand appartement Paris',
+  address: '19 rue de Naples 75008 Paris',
+  description: 'Très joli',
+  price_per_night: 60,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Très beau',
+  price_per_night: 70,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Petit appartement charmant',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Très spacieux',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Taudis',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Très lumineu',
+  price_per_night: 80,
+  number_of_guests: 4
+)# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
